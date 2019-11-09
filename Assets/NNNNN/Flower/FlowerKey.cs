6bbfd5c2eeec0f5205eac7c06e513e2a98bfea3a
Shawn <botider@gmail.com>
2019-11-09 15:20:03 +0800

    萬花筒!!

---
+++ Assets/NNNNN/Flower/FlowerKey.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerKey : MonoBehaviour
+{
+    public string password;
+    public bool findDoor;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var c = collision.GetComponent<FlowerDoor>();
+        if(c.password == this.password)
+        {
+            findDoor = true;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //if(Input.GetKey(PlayerKeyA))
+       //{
+       //    this.transform.localEulerAngles += new Vector3(0,0,1) *deltaAngle;
+       //}
+       //if (Input.GetKey(PlayerKeyB))
+       //{
+       //    this.transform.localEulerAngles -= new Vector3(0, 0, 1) * deltaAngle;
+       //}
+
+
+    }
+}
---
+++ Assets/NNNNN/Flower/Party.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class Party : MonoBehaviour
+{
+
+    PostProcessVolume ps;
+    ColorGrading outSetting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<PostProcessVolume>();
+        ps.sharedProfile.TryGetSettings<ColorGrading>(out ColorGrading outSetting);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(outSetting)
+        {
+            outSetting.hueShift.value = Random.Range(0, 1);
+        }
+    }
+}
