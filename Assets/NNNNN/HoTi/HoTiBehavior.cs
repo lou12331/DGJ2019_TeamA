0b8d8169911a32c0543e5c33d3bcb2e14fc59fa3
Shawn <botider@gmail.com>
2019-11-09 18:06:52 +0800

    合體temp

---
+++ Assets/NNNNN/HoTi/HoTiBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HoTiBehavior : MonoBehaviour
+{
+    public List<HoTiCollider> co;
+    public bool canMove=true;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        var cc = GetComponentsInChildren<HoTiCollider>();
+        co = cc.ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool allHit = true;
+        foreach (var c in co)
+        {
+
+            allHit = c.IsHit && allHit;
+        }
+        if(allHit)
+        {
+            Debug.Log("YA");
+            canMove = false;
+        }
+    }
+}
