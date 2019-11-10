24f0c20169ea46a627b353e00ef51b92045281cb
Shawn <botider@gmail.com>
2019-11-10 08:12:53 +0800

    火球

---
+++ Assets/NNNNN/fireball/FireBallBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallBehavior : MonoBehaviour
+{
+    public Vector2 force;
+    Rigidbody2D rig;
+    public string tag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.AddForce(force);
+        Destroy(this.gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var c = collision.gameObject.GetComponent<FireBallBehavior>();
+        var d = collision.gameObject.GetComponent<FireBallInput>();
+        if (c)
+        {
+            if (c.tag != this.tag)
+            {
+                Destroy(this.gameObject);
+            }
+            Debug.Log("FireBall contact FireBall");
+
+        }
+
+        if (d)
+        {
+            if (d.tag != this.tag)
+            {
+                d.GetComponent<Rigidbody2D>().AddForce(force);
+                Debug.Log("FireBall contact Player");
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+}
---
+++ Assets/NNNNN/fireball/FireBallInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallInput : MonoBehaviour
+{
+
+    public Sprite[] sps;
+    public bool p1;
+    public GameObject fireball;
+    public Transform fireballSpawn;
+    public string tag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (p1)
+        {
+            if (SYS.input.key_confirm_a || SYS.input.key_cancel_a)
+            {
+                var go = Instantiate(fireball, fireballSpawn);
+                go.SetActive(true);
+            }
+        }
+        else
+        {
+            if (SYS.input.key_confirm_b || SYS.input.key_cancel_b)
+            {
+                var go = Instantiate(fireball, fireballSpawn);
+                go.SetActive(true);
+            }
+        }
+       
+    }
+}
