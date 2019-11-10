6f8279114c320ed17cdd296663c9dedc8a8a1ebb
Shawn <botider@gmail.com>
2019-11-10 14:03:20 +0800

    GG UI

--- Assets/five/GGmove.cs
+++ Assets/five/GGmove.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class GGmove : MonoBehaviour
 {
@@ -9,30 +9,51 @@ public class GGmove : MonoBehaviour
     public GameObject Box;
     public List<GameObject> lsit;
     public static int index;
+    public Text text;
     void Start() 
     {
         index = Random.Range(0, 5);
         Box.transform.position = lsit[index].transform.position;
-        
+        text.text = "";
     }
     private void Update()
     {
         switch (index)
         {
             case 0:
-                if (Input.GetKeyDown(KeyCode.W)) Debug.Log("P1wim");
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    Debug.Log("P1wim");
+                    text.text = "P1 Win";
+                }
                 break;
             case 1:
-                if (Input.GetKeyDown(KeyCode.S)) Debug.Log("P1wim");
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    Debug.Log("P1wim");
+                    text.text = "P1 Win";
+                }
                 break;
             case 2:
-                if (Input.GetKeyDown(KeyCode.A)) Debug.Log("P1wim");
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    Debug.Log("P1wim");
+                    text.text = "P1 Win";
+                }
                 break;
             case 3:
-                if (Input.GetKeyDown(KeyCode.D)) Debug.Log("P1wim");
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    Debug.Log("P1wim");
+                    text.text = "P1 Win";
+                }
                 break;
             case 4:
-                if (Input.GetKeyDown(KeyCode.BackQuote)) Debug.Log("P1wim");
+                if (Input.GetKeyDown(KeyCode.BackQuote))
+                {
+                    Debug.Log("P1wim");
+                    text.text = "P1 Win";
+                }
                 break;
         }
     }
--- Assets/five/GGmove_p2.cs
+++ Assets/five/GGmove_p2.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GGmove_p2 : MonoBehaviour
 {
@@ -8,11 +9,13 @@ public class GGmove_p2 : MonoBehaviour
     public GameObject Box;
     public List<GameObject> lsit;
     public int index;
+    public Text text;
     void Start()
     {
         index = GGmove.index;
         index = Random.Range(0, 5);
         Box.transform.position = lsit[index].transform.position;
+        text.text = "";
 
     }
     private void Update()
@@ -20,19 +23,39 @@ public class GGmove_p2 : MonoBehaviour
         switch (index)
         {
             case 0:
-                if (Input.GetKeyDown(KeyCode.UpArrow)) Debug.Log("P2wim");
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    Debug.Log("P2wim");
+                    text.text = "P2 Win";
+                }
                 break;
             case 1:
-                if (Input.GetKeyDown(KeyCode.DownArrow)) Debug.Log("P2wim");
+                if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    Debug.Log("P2wim");
+                    text.text = "P2 Win";
+                }
                 break;
             case 2:
-                if (Input.GetKeyDown(KeyCode.LeftArrow)) Debug.Log("P2wim");
+                if (Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    Debug.Log("P2wim");
+                    text.text = "P2 Win";
+                }
                 break;
             case 3:
-                if (Input.GetKeyDown(KeyCode.RightArrow)) Debug.Log("P2wim");
+                if (Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    Debug.Log("P2wim");
+                    text.text = "P2 Win";
+                }
                 break;
             case 4:
-                if (Input.GetKeyDown(KeyCode.Comma)) Debug.Log("P2wim");
+                if (Input.GetKeyDown(KeyCode.Comma))
+                {
+                    Debug.Log("P2wim");
+                    text.text = "P2 Win";
+                }
                 break;
         }
     }
