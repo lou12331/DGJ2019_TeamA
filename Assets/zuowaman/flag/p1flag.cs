8bd29e27aece50031ff9f0727af3b681e6aed965
zuowaman <evan9003@gmail.com>
2019-11-10 13:08:17 +0800

    audio

--- Assets/zuowaman/flag/p1flag.cs
+++ Assets/zuowaman/flag/p1flag.cs
@@ -2,42 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class p2flag : MonoBehaviour
+public class p1flag : MonoBehaviour
 {
-
-    public p1flag p1;
+    public p2flag p2;
     public bool IsGameEnd = false;
+    public AudioSource audio_2;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         if (IsGameEnd == true) EndGame();
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        { 
             transform.Rotate(0, 0, -3);
-        if (Input.GetKeyDown(KeyCode.D))
+            audio_2.Play();
+        }
+            
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             transform.Rotate(0, 0, -3);
+            audio_2.Play();
+        }
         if (Input.GetKeyDown(KeyCode.Space)) Time.timeScale = 0;
 
         if (transform.eulerAngles.z > 90)
             transform.Rotate(0, 0, -0.5f);
-        if (transform.eulerAngles.z < 90 || transform.eulerAngles.z > 0)
+        if (transform.eulerAngles.z < 90 || transform.eulerAngles.z >0)
             transform.Rotate(0, 0, 0.5f);
         //Debug.Log(transform.localEulerAngles.z);
-        if (transform.localEulerAngles.z > 300)
+        if (transform.localEulerAngles.z > 300) 
         {
-            Debug.Log("P2 win");
+            Debug.Log("P1 win");
             this.enabled = false;
             IsGameEnd = true;
-            p1.IsGameEnd = true;
+            p2.IsGameEnd = true;
+
         }
         //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z % 180);
-
-
+        
+            
     }
     public void EndGame()
     {
